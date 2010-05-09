b8a2c52145cd9fdbbf7a0987abc8684da7d4ab46
theanti9 <the.anti.9@gmail.com>
2010-05-09 03:47:03 -0400

    Made Module class load modules and get the UrlMap from the dll's ModuleMap class. Added InvalidModuleMapException.cs

---
+++ irek/Modules/Errors/InvalidModuleMapException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace irek.Modules.Errors
+{
+    public class InvalidModuleMapException : System.Exception
+    {
+        public InvalidModuleMapException() : base() { }
+        public InvalidModuleMapException(string message) : base(message) { }
+    }
+}
--- irek/Modules/Module.cs
+++ irek/Modules/Module.cs
@@ -4,14 +4,16 @@ using System.IO;
 using System.Reflection;
 using System.Text;
 using irek.Configuration;
-using irek.Configuration.Errors
-
+using irek.Configuration.Errors;
+using irek.Modules.Errors;
+using libirek.Urls;
 namespace irek.Modules
 {
     public class Module
     {
         public string ModulePath;
         public Assembly ModuleAssembly;
+        public UrlMap<UrlMapItem> UrlMap;
 
         public Module(string modulepath)
         {
@@ -25,6 +27,22 @@ namespace irek.Modules
                 throw new NoSuchModuleException("Error: No such module at '" + ModulePath + "'.");
             }
             ModuleAssembly = Assembly.LoadFile(ModulePath);
+            int lastslash = ModulePath.LastIndexOf(@"\");
+            string assemblynamespace = ModulePath.Substring(lastslash, ModulePath.LastIndexOf('.') - lastslash);
+            Type t = ModuleAssembly.GetType(assemblynamespace+".ModuleMap");
+            if (t != null)
+            {
+                MethodInfo m = t.GetMethod("GetUrlMap");
+                if (m != null)
+                {
+                    object[] mparams = new object[0];
+                    UrlMap = (UrlMap<UrlMapItem>)m.Invoke(null, mparams);
+                }
+            }
+            else
+            {
+                throw new InvalidModuleMapException("Error: The ModuleMap class is missing or incorrect!");
+            }
         }
     }
 }
