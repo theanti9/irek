e509422a489a73b151e7954ca38a04b48a096c18
theanti9 <the.anti.9@gmail.com>
2010-07-11 21:53:46 -0400

    old libirek changes

---
+++ irek/Util.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace irek
+{
+	public static class Util
+	{
+		public static string RandomString(int length)
+		{
+			char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+			StringBuilder sb = new StringBuilder(length);
+			for (int i = 0; i < length; i++)
+			{
+				sb.Append(chars[i]);
+			}
+			return sb.ToString();
+		}
+	}
+}
---
+++ libirek/SQL/Db.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace libirek.SQL
+{
+	public class Db
+	{
+		public SqlConnection Connection;
+		public string Name;
+		public Db(string username, string password, string serverurl, string database)
+		{
+			Name = database;
+			Connection = new SqlConnection("user id=" + username + 
+				";password=" + password + 
+				";server=" + serverurl + 
+				";Trusted_Connection=yes;Database=" + database + 
+				";connection timeout=30");
+
+		}
+	}
+}
